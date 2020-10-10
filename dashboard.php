067038ca4662b83a561b50cd250a7a64d310b247
D. Chavez <dionisiochavez@gmail.com>
2020-10-10 06:59:30 -0400

    Adding datagrid

--- dashboard.php
+++ dashboard.php
@@ -1,1 +1,49 @@
-<h1>Dashboard</h1>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dashboard</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+   <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+   <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+   <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body class="bg-login">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-10 offset-lg-2">
+				<div class="p-2 rounded bg-light mt-5">
+					
+				
+					<table class="table table-striped">
+					  <thead>
+					    <tr>
+					      <th scope="col">ID</th>
+					      <th scope="col">Agent</th>
+					      <th scope="col">Score</th>
+					      <th scope="col">Rated By</th>
+					    </tr>
+					  </thead>
+					  <tbody>
+					    <tr>
+					      <th scope="row">1</th>
+					      <td>Mark</td>
+					      <td>86</td>
+					      <td>Mark Zuckerberg</td>
+					    </tr>
+					    <tr>
+					      <th scope="row">2</th>
+					      <td>Jacob</td>
+					      <td>91</td>
+					      <td>steve jobs</td>
+					    </tr>
+					    
+					  </tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+		
+	</div>
+</body>
+</html>
