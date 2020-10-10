86a931abfc4885ab07a312106b8059441c376dac
D. Chavez <dionisiochavez@gmail.com>
2020-10-10 08:18:46 -0400

    Adding dashboard top bar

--- dashboard.php
+++ dashboard.php
@@ -9,18 +9,36 @@
    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body class="bg-login">
+	<div class="top-bar">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<img class="user-img rounded-circle" alt="100x100" src="assets/img/user.jpg" data-holder-rendered="true">
+					<div class="user-data">
+						<div class="name">Dionisio Chavez</div>
+						<div class="position">Web Developer</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="d-flex align-items-center h-100 float-right">
+						<button type="button" class="btn btn-success">New Agent</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 	<div class="container">
 		<div class="row">
-			<div class="col-lg-10 offset-lg-2">
+			<div class="col-lg-8 offset-lg-2">
 				<div class="p-2 rounded bg-light mt-5">
 					<table class="table table-striped">
 					  <thead>
 					    <tr>
-					      <th scope="col">ID</th>
-					      <th scope="col">Agent</th>
-					      <th scope="col">Score</th>
-					      <th scope="col">Rated By</th>
-					      <th scope="col" style="width: 140px">Actions</th>
+					      <th scope="col" class="border-0">ID</th>
+					      <th scope="col" class="border-0">Agent</th>
+					      <th scope="col" class="border-0">Score</th>
+					      <th scope="col" class="border-0">Rated By</th>
+					      <th scope="col" class="border-0" style="width: 140px">Actions</th>
 
 					    </tr>
 					  </thead>
