9896d12166de743ad4144bccafc52f61af93405c
D. Chavez <dionisiochavez@gmail.com>
2020-10-12 15:54:21 -0400

    Adding agents table

--- OutPLEX.sql
+++ OutPLEX.sql
@@ -11,11 +11,30 @@
  Target Server Version : 50726
  File Encoding         : 65001
 
- Date: 09/10/2020 00:21:38
+ Date: 12/10/2020 15:53:32
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for agents
+-- ----------------------------
+DROP TABLE IF EXISTS `agents`;
+CREATE TABLE `agents` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `score` int(3) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of agents
+-- ----------------------------
+BEGIN;
+INSERT INTO `agents` VALUES (1, 'Mark Zuckerberg', 86);
+INSERT INTO `agents` VALUES (2, 'Steve Jobs', 93);
+COMMIT;
 
 -- ----------------------------
 -- Table structure for users
@@ -27,6 +46,13 @@ CREATE TABLE `users` (
   `password` varchar(100) NOT NULL,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+BEGIN;
+INSERT INTO `users` VALUES (1, 'chavez', '8df2081e1154e8e16a7e78e477c44949', 'Dionisio Chavez');
+COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
